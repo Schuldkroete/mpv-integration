da3242d333d02b7cca2d6f785904a8130407612c
Schuldkröte <18383242+Schuldkroete@users.noreply.github.com>
2023-11-06 09:54:01 +0100

    remove unnecessary sample code; add needed settings and command functions

--- main.ts
+++ main.ts
@@ -1,81 +1,43 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface MpvIntegrationPluginSettings {
+	mpv_domain_filter: string;
+	mpv_file_extension_filter: string;
+	mpv_url_filter: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MpvIntegrationPluginSettings = {
+	mpv_domain_filter: 'www.youtube.com',
+	mpv_file_extension_filter: '.mp4,.mkv',
+	mpv_url_filter: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MpvIntegrationPlugin extends Plugin {
+	settings: MpvIntegrationPluginSettings;
+
+	async playInMpv(view: MarkdownView, editor: Editor) {
+
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.addRibbonIcon('play-circle', 'Play in mpv', (evt: MouseEvent) => {
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if (view) {
+				this.playInMpv(view, view.editor);
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'mpv-integration-play-in-mpv',
+			name: 'Play current file in mpv',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.playInMpv(view, editor);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new MpvIntegrationSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,26 +53,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class MpvIntegrationSettingTab extends PluginSettingTab {
+	plugin: MpvIntegrationPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MpvIntegrationPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +66,46 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+		const mpv_domain_filter_setting = new Setting(containerEl);
+		mpv_domain_filter_setting
+			.setName('Domain Filter')
+			.setDesc('List of Domains that should be sent to mpv.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('domains')
+				.setValue(this.plugin.settings.mpv_domain_filter)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.mpv_domain_filter = value;
 					await this.plugin.saveSettings();
-				}));
+				}
+			)
+		);
+
+		const mpv_file_extension_filter_setting = new Setting(containerEl);
+		mpv_file_extension_filter_setting
+			.setName('File Type Filter')
+			.setDesc('List of File Extensions that should be sent to mpv.')
+			.addText(text => text
+				.setPlaceholder('extensions')
+				.setValue(this.plugin.settings.mpv_file_extension_filter)
+				.onChange(async (value) => {
+					this.plugin.settings.mpv_file_extension_filter = value;
+					await this.plugin.saveSettings();
+				}
+			)
+		);
+
+		const mpv_url_filter_setting = new Setting(containerEl);
+		mpv_url_filter_setting
+			.setName('URL Filter')
+			.setDesc('List of URLs that should be sent to mpv.')
+			.addText(text => text
+				.setPlaceholder('URLs')
+				.setValue(this.plugin.settings.mpv_url_filter)
+				.onChange(async (value) => {
+					this.plugin.settings.mpv_url_filter = value;
+					await this.plugin.saveSettings();
+				}
+			)
+		);
 	}
 }
