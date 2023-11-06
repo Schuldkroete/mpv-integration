9790fd5ea5fc76a4617f3b50b532b6720ca61da2
Schuldkröte <18383242+Schuldkroete@users.noreply.github.com>
2023-11-06 10:21:33 +0100

    cleanup settings (likely final); act on MarkdownView instead of editor

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface MpvIntegrationPluginSettings {
 	mpv_domain_filter: string;
@@ -7,16 +7,18 @@ interface MpvIntegrationPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MpvIntegrationPluginSettings = {
-	mpv_domain_filter: 'www.youtube.com',
-	mpv_file_extension_filter: '.mp4,.mkv',
+	mpv_domain_filter: 'youtube.com\nyoutu.be\nreddit.com\ntwitch.tv',
+	mpv_file_extension_filter: 'mp4\nwebm\nmkv\nflv\navi\nwmv\nmpg\nmpeg\n3gp;',
 	mpv_url_filter: ''
 }
 
 export default class MpvIntegrationPlugin extends Plugin {
 	settings: MpvIntegrationPluginSettings;
 
-	async playInMpv(view: MarkdownView, editor: Editor) {
-
+	async playInMpv(view: MarkdownView) {
+		const mod = new Modal(this.app);
+		mod.contentEl.textContent = view.getViewData();
+		mod.open()
 	}
 
 	async onload() {
@@ -25,24 +27,31 @@ export default class MpvIntegrationPlugin extends Plugin {
 		this.addRibbonIcon('play-circle', 'Play in mpv', (evt: MouseEvent) => {
 			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 			if (view) {
-				this.playInMpv(view, view.editor);
+				this.playInMpv(view);
 			}
 		});
 
 		this.addCommand({
 			id: 'mpv-integration-play-in-mpv',
 			name: 'Play current file in mpv',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				this.playInMpv(view, editor);
+			checkCallback: (checking: boolean) => {
+				// check if a document is open
+				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+				if (markdownView) {
+					if (!checking) {
+						this.playInMpv(markdownView);
+					}
+					return true
+				}
+				return false;
 			}
 		});
 
 		this.addSettingTab(new MpvIntegrationSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -69,43 +78,49 @@ class MpvIntegrationSettingTab extends PluginSettingTab {
 		const mpv_domain_filter_setting = new Setting(containerEl);
 		mpv_domain_filter_setting
 			.setName('Domain Filter')
-			.setDesc('List of Domains that should be sent to mpv.')
-			.addText(text => text
-				.setPlaceholder('domains')
-				.setValue(this.plugin.settings.mpv_domain_filter)
-				.onChange(async (value) => {
-					this.plugin.settings.mpv_domain_filter = value;
-					await this.plugin.saveSettings();
-				}
-			)
+			.setDesc('List of allowed Domains. (one per line)')
+			.addTextArea(text => {
+				text.setValue(this.plugin.settings.mpv_domain_filter)
+					.onChange(async (value) => {
+						this.plugin.settings.mpv_domain_filter = value;
+						await this.plugin.saveSettings();
+					}
+				);
+				text.inputEl.rows = 10;
+				text.inputEl.cols = 25;
+			}
 		);
 
 		const mpv_file_extension_filter_setting = new Setting(containerEl);
 		mpv_file_extension_filter_setting
 			.setName('File Type Filter')
-			.setDesc('List of File Extensions that should be sent to mpv.')
-			.addText(text => text
-				.setPlaceholder('extensions')
-				.setValue(this.plugin.settings.mpv_file_extension_filter)
-				.onChange(async (value) => {
-					this.plugin.settings.mpv_file_extension_filter = value;
-					await this.plugin.saveSettings();
-				}
-			)
+			.setDesc('List of allowed File Extensions. (one per line)')
+			.addTextArea(text => {
+				text.setValue(this.plugin.settings.mpv_file_extension_filter)
+					.onChange(async (value) => {
+						this.plugin.settings.mpv_file_extension_filter = value;
+						await this.plugin.saveSettings();
+					}
+				);
+				text.inputEl.rows = 10;
+				text.inputEl.cols = 25;
+			}
 		);
 
 		const mpv_url_filter_setting = new Setting(containerEl);
 		mpv_url_filter_setting
 			.setName('URL Filter')
-			.setDesc('List of URLs that should be sent to mpv.')
-			.addText(text => text
-				.setPlaceholder('URLs')
-				.setValue(this.plugin.settings.mpv_url_filter)
-				.onChange(async (value) => {
-					this.plugin.settings.mpv_url_filter = value;
-					await this.plugin.saveSettings();
-				}
-			)
+			.setDesc('Only needed, in a few cases: List of allowed URLs. (regex, one per line)')
+			.addTextArea(text => {
+				text.setValue(this.plugin.settings.mpv_url_filter)
+					.onChange(async (value) => {
+						this.plugin.settings.mpv_url_filter = value;
+						await this.plugin.saveSettings();
+					}
+				);
+				text.inputEl.rows = 10;
+				text.inputEl.cols = 25;
+			}
 		);
 	}
 }
